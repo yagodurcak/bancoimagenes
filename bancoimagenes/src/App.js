ceac2089d1dddc933f6219df1fa94302b4c26908
yagodurcak <mauridurcak@gmail.com>
2021-11-15 20:06:24 -0300

    finalizado 15/11

--- bancoimagenes/src/App.js
+++ bancoimagenes/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Formulario from './components/Formulario';
 import Listadoimg from './components/Listadoimg';
@@ -21,19 +21,86 @@ font-size: 90px;
 margin-bottom  : 50px;
 `
 
+const Boton = styled.button`
+    height: 60px;
+    width: 300px;
+    background-color: salmon;
+    margin: 10px 10px;
+    font-size: 20px;
+    font-weight: 700;
+    color: white;
+`
+
 function App() {
 
   const [busqueda, setBusqueda] = useState("");
-  console.log(busqueda);
+  const [rtdo, setRtdo] = useState([])
+  const [pagActual, setPagActual] = useState(1);
+  const [totalPaginas, setTotalPaginas] = useState(1);
+
+  useEffect(() => {
+    const Apirtdo = async () => {
+      if (busqueda === "") return;
+
+      const imgporpag = 20;
+      const key = "23281847-15060561ca46cab48b434ee84"
+      const apiUrl= `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imgporpag}&page=${pagActual}`
+
+      const respuesta = await fetch(apiUrl);
+      const respuesta2 = await respuesta.json();
+      setRtdo(respuesta2.hits)
+
+      // contando cantidad de paginas a mostrar 
+
+      const totalPag = Math.ceil(respuesta2.totalHits / imgporpag);
+      setTotalPaginas(totalPag)
+
+      // mover pantalla hacia arriba
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({ behavior: 'smooth' })
+     
+    };
+    Apirtdo()
+  }, [busqueda, pagActual]);
+
+  const PaginaAnterior = () => {
+    
+    if (pagActual > 1) {
+      const nuevaPagActual = pagActual - 1 ;
+    setPagActual(nuevaPagActual)
+    }
+  }
+
+  const PaginaPosterior = () => {
+
+    if (totalPaginas > pagActual) {
+      
+      const nuevaPagActual = pagActual + 1 ;
+      setPagActual(nuevaPagActual)
+    }
+  }
+  
+  
+
   return (
     <div>
-      <Container>
+      <Container className="jumbotron">
         <div>
          <Title className="title text-white text-center">Buscador de Imagenes</Title>
           <Formulario setBusqueda={setBusqueda}/>
         </div>
       </Container>
-        <Listadoimg/>
+      
+        <Listadoimg rtdo={rtdo}/>
+
+
+        <div className= "flex justify-center mt-20">
+        {(pagActual === 1)  ? null : (<Boton onClick={PaginaAnterior}>&laquo; Anterior </Boton>)}
+        {(pagActual === totalPaginas)  ? null : (<Boton onClick={PaginaPosterior}>Siguiente &raquo;</Boton>) }
+          
+          
+        </div>
+        
     </div>
   );
 }
--- bancoimagenes/src/components/Listadoimg.js
+++ bancoimagenes/src/components/Listadoimg.js
@@ -16,42 +16,21 @@ const Boton = styled.button`
     color: white;
 `
 
-function Listadoimg() {
+function Listadoimg({rtdo}) {
+
+  
     return (
         <div className="grid grid-cols-4 gap-4 mx-40 mt-10">
-            <div className= "bg-gray-400 p-10">
-                <Imagen src={prod} alt="" />
+            {rtdo.map(casa => (  <div className= "bg-gray-400 p-10" key={rtdo.id}>
+                <Imagen src={casa.previewURL} alt="" />
                 <div className="grid justify-items-center mt-5">
-                    <h3 className="text-center text-xl font-bold text-white">Vistas</h3>
-                    <h3 className="text-center text-xl font-bold text-white">Vistas</h3>
-                    <Boton>Ver Imagen</Boton>
+                    <h3 className="text-center text-xl font-bold text-white">Previews: {casa.previewWidth}</h3>
+                    <h3 className="text-center text-xl font-bold text-white">Likes: {casa.likes}</h3>
+                    <Boton><a  href={casa.largeImageURL} target="_blank" rel="noopener noreferrer">Ver Imagen</a></Boton>
                 </div>
-            </div>
-            <div className= "bg-gray-400 p-10">
-                <Imagen src={prod} alt="" />
-                <div className="grid justify-items-center mt-5">
-                    <h3 className="text-center text-xl font-bold text-white">Vistas</h3>
-                    <h3 className="text-center text-xl font-bold text-white">Vistas</h3>
-                    <Boton>Ver Imagen</Boton>
-                </div>
-            </div>
-            <div className= "bg-gray-400 p-10">
-                <Imagen src={prod} alt="" />
-                <div className="grid justify-items-center mt-5">
-                    <h3 className="text-center text-xl font-bold text-white">Vistas</h3>
-                    <h3 className="text-center text-xl font-bold text-white">Vistas</h3>
-                    <Boton>Ver Imagen</Boton>
-                </div>
-            </div>
-            <div className= "bg-gray-400 p-10">
-                <Imagen src={prod} alt="" />
-                <div className="grid justify-items-center mt-5">
-                    <h3 className="text-center text-xl font-bold text-white">Vistas</h3>
-                    <h3 className="text-center text-xl font-bold text-white">Vistas</h3>
-                    <Boton>Ver Imagen</Boton>
-                </div>
-            </div>
-   
+            </div>))}
+          
+             
        
         </div>
     )
